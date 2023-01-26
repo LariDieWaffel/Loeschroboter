c696337a9e2288a45ba97248009571b78c1f70c5
LariDieWaffel <gewitterretriever@gmail.com>
2023-01-26 08:22:51 +0100

    No water and driving possible, more commands

--- Loeschroboter.ino
+++ Loeschroboter.ino
@@ -22,6 +22,8 @@ int echo = 6;
 long duration = 0;
 long distance = 0;
 double percentage = 0;
+double heightTank = 16; //Höhe Tank (cm)
+int deviation = 2; // Höhe Sensor über maximalem Wasserspiegel
 
 void setup() 
 {
@@ -43,6 +45,9 @@ void setup()
 
 void forward ()
 {
+  //Pumpe ausstellen
+  digitalWrite (pump, LOW);
+
   //Motoransteuerung zum vorwärts Fahren
   digitalWrite (motor1, HIGH);
   digitalWrite (motor2, HIGH);
@@ -52,6 +57,9 @@ void forward ()
 
 void stop ()
 {
+  //Pumpe ausstellen
+  digitalWrite (pump, LOW);
+
   //Motoransteuerung zum Stoppen
   digitalWrite (motor1, LOW);
   digitalWrite (motor2, LOW);
@@ -61,6 +69,9 @@ void stop ()
 
 void left ()
 {
+  //Pumpe ausstellen
+  digitalWrite (pump, LOW);
+
   //Motoransteuerung zum nach links Fahren
   digitalWrite (motor1, LOW);
   digitalWrite (motor2, HIGH);
@@ -70,6 +81,9 @@ void left ()
 
 void right ()
 {
+  //Pumpe ausstellen
+  digitalWrite (pump, LOW);
+
   //Motoransteuerung zum nach rechts Fahren
   digitalWrite (motor1, HIGH);
   digitalWrite (motor2, LOW);
@@ -79,19 +93,26 @@ void right ()
 
 void extinguish () 
 {
+  //alle Motoren stoppen
+  digitalWrite (motor1, LOW);
+  digitalWrite (motor2, LOW);
+  digitalWrite (motor3, LOW);
+  digitalWrite (motor4, LOW);
+
+  //Pumpe anstellen
   digitalWrite (pump, HIGH);
   Serial.println ("Wasser läuft!");
 }
 
 void loop() 
 {
-  //Bedingungen die in den Funktionen resultieren
+  //Bedingungen zum Fahren, die in den Funktionen resultieren
 
   if (Serial.available () ) //wenn Daten gegeben sind
   {
-    char command = Serial.read (); //legt char an mit dem, was die GUI sendet
+    char command = Serial.read (); //legt char an mit dem, was das GUI sendet
 
-    if (command == 'w' || 'VK_UP') //überprüft ob die GUI w oder Pfeil nach oben gesendet hat
+    if (command == 'w' || 'VK_UP') //überprüft ob das GUI w oder Pfeil nach oben gesendet hat
     {
       forward (); //Func forward wird ausgeführt
       Serial.println ("Wir fahren vorwärts!");
@@ -119,51 +140,55 @@ void loop()
     else if (command == 'q')
     {
       extinguish();
+      Serial.println ("Wir löschen!");
     }
 
-    else
+    else //falls eine Taste genutzt wird, die keine Funktion hat
     {
       Serial.println ("Diese Eingabe hat keine Funktion!");
     }
 
   }
-
+  
+  //Stellt Messgerät für 10sek an um eine Messung durchzuführen
   digitalWrite (trigger, LOW);
   delay (5); 
   digitalWrite (trigger, HIGH);
   delay (10); 
   digitalWrite (trigger, LOW);
-
+  
+  //Berechnung des Wasserstandes
   duration = pulseIn (echo, HIGH);
-  distance = (duration/2) * 0.03432; 
+  distance = (duration/2) * 0.03432; //Berechnet Entfernung vom Sensor zur Wasseroberfläche
 
-  double heightTank = 18.3; //Höhe Tank (cm)
-  int deviation = 2; // Höhe Sensor über maximalem Wasserspiegel
+  percentage = 100-(((distance-deviation)*100)/heightTank); //Rechnet die Entfernung um in eine Prozentzahl abhängig von der max. Füllung
 
-  percentage = 100-(((distance-deviation)*100)/heightTank);
-
-  if (distance >= 500 || distance <= 0) 
+  if (distance >= 500 || distance <= 0) //wenn der Tank komplett leer oder überfüllt ist
   {
     Serial.println("Kein Messwert vorhanden");
   }
 
   else
   {
-    // Abstufungen 
+    //serielle Ausgabe in Abhängigkeit von der berechnet Prozentzahl, um den Wasserstand bekannt zu geben
+
     if (percentage < 25) 
     {
-      Serial.println("Sehr geringer Wasserstand!");
-      Serial.println("Bitte Wasser auffüllen.");     
+      //Serial.println("Sehr geringer Wasserstand!");
+      //Serial.println("Bitte Wasser auffüllen."); 
+      Serial.println("m");    
     }
 
     else if (percentage < 50) 
     {
-      Serial.println("Wasserstand gering.");
+      //Serial.println("Wasserstand gering!");
+      Serial.println("n");
     }
 
     else 
     {
-      Serial.println("Es ist ausreichend Wasser da.");
+      //Serial.println("Es ist ausreichend Wasser da.");
+      Serial.println("b");
     }
   }
 
